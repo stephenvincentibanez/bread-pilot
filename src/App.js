bbce2043c60c34a323d6c9c1113015b73667a220
stephenvincentibanez <stephenvincentibanez@gmail.com>
2021-02-01 16:35:14 -0800

    some links working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import {SiApplemusic} from 'react-icons/si'
+import {SiSpotify} from 'react-icons/si'
+import {SiYoutube} from 'react-icons/si'
+import {SiInstagram} from 'react-icons/si'
+
 
 function App() {
   return (
     <div>
       <h1>Bread Pilot</h1>
       <div className="navBar">
-        <h3>Music</h3>
+        <a href="https://breadpilotband.bandcamp.com/">
+          <h3>Music</h3>
+        </a>
         {/* link to bandcamp */}
         <h3>Booking</h3>
         {/* component with booking email  */}
@@ -13,6 +20,14 @@ function App() {
         {/* link to bandcamp merch */}
         <h3>Live</h3>
         {/* component with listings of shows  */}
+      </div>
+      <div className="links">
+        <SiApplemusic/>
+        <SiSpotify/>
+        <a href="https://www.youtube.com/user/breadpilotband">
+          <SiYoutube/>
+        </a>
+        <SiInstagram/>
       </div>
     </div>
   );
