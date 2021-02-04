74d28d543a6f360c717e254d58bfd8e4a8b85d44
stephenvincentibanez <stephenvincentibanez@gmail.com>
2021-02-03 20:35:09 -0800

    additional styling

--- src/components/Linkbar.js
+++ src/components/Linkbar.js
@@ -8,19 +8,19 @@ import {FaBandcamp} from 'react-icons/fa'
 const Linkbar = () => {
     return (
         <div className="links">
-            <a href="https://itunes.apple.com/us/artist/bread-pilot/1029036444">
+            <a className="link" href="https://itunes.apple.com/us/artist/bread-pilot/1029036444">
                 <SiApplemusic/>
             </a>
-            <a href="https://open.spotify.com/artist/6WX9FRYQIYRRTNPZaPFQ74" id="spotify">
+            <a className="link" href="https://open.spotify.com/artist/6WX9FRYQIYRRTNPZaPFQ74" id="spotify">
                 <SiSpotify/>
             </a>
             {/* <a href="https://www.youtube.com/user/breadpilotband">
                 <SiYoutube/>
             </a> */}
-            <a href="https://www.instagram.com/bread_pilot/?hl=en">
+            <a className="link" href="https://www.instagram.com/bread_pilot/?hl=en">
                 <SiInstagram/>         
             </a>
-            <a href="https://breadpilotband.bandcamp.com/" id="bandcamp">
+            <a className="link" href="https://breadpilotband.bandcamp.com/" id="bandcamp">
                 <FaBandcamp/>
             </a>
         </div>
